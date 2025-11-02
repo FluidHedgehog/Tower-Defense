ac0ef870035ec37847cd0a030f85e0781a058459
Artur Myszkorowski <artur.myszkorowski@gmail.com>
2025-11-02 10:52:05 +0100

    Book + options

---
+++ Assets/GameLogic/Menu/InfoBookActions.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class InfoBookActions : MonoBehaviour
+{
+    public int currentPage;
+
+    public GameObject[] pages;
+
+    public void OpenFirstPage()
+    {
+        currentPage = 0;
+        pages[0].gameObject.SetActive(true);
+    }
+
+    public void FlipPage(bool nextPage)
+    {
+        if (nextPage && currentPage < (pages.Length - 1))
+        {
+            pages[currentPage].gameObject.SetActive(false);
+            currentPage += 1;
+            pages[currentPage].gameObject.SetActive(true);
+        }
+        else if (!nextPage && currentPage > 0)
+        {
+            pages[currentPage].gameObject.SetActive(false);
+            currentPage -= 1;
+            pages[currentPage].gameObject.SetActive(true);
+        }
+    }
+
+    public void CloseBook()
+    {
+        gameObject.SetActive(false);
+    }
+}
